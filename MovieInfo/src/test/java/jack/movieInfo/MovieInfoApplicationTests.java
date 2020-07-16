fda2e1f0593a4a57d03ba0db797f2a5232151218
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-16 11:23:20 -0400

    1. Created Three spring boot projects
    Dependencies for each project is as follow
    
    MovieCatalog: Eureka Discovery Client, Hystrix Dashboard, Hystrix, Spring boot actuator, Spring Reactive Web, Spring web
    
    MovieInfo: Spring web, Eureka Discovery Client
    
    MovieRating: Spring web, Eureka Discovery Client

---
+++ MovieInfo/src/test/java/jack/movieInfo/MovieInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package jack.movieInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
