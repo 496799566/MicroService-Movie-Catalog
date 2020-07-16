76bac67786f838651a2cfc53418d353c9e2ae9f9
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-16 12:33:28 -0400

    Update MovieCatalogApplication.java
    Implemented Hystrix the CircuitBreaker and a loadBalanced RestTemplate Bean

--- MovieCatalog/src/main/java/jack/movieCatalog/MovieCatalogApplication.java
+++ MovieCatalog/src/main/java/jack/movieCatalog/MovieCatalogApplication.java
@@ -2,10 +2,39 @@ package jack.movieCatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		
+		clientHttpRequestFactory.setReadTimeout(3000);
+		
+		return new RestTemplate(clientHttpRequestFactory);
+		
+	}
+	
+	/*
+	@Bean
+	public WebClient.Builder getRestClientBuilder() {
+		return WebClient.builder();
+	}
+	*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogApplication.class, args);
 	}
