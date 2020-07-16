28a5173969dc1d030c0a5286d69857f2ddd4624a
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-16 12:09:58 -0400

    3. Created MovieInfo API
    An api that make a call to the Movie Database(TMDb) with a given movie id, then retrieve 1. movie name 2. A descriptive overview of the movie

---
+++ MovieInfo/src/main/java/jack/movieInfo/resource/MovieResource.java
@@ -0,0 +1,31 @@
+package jack.movieInfo.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import jack.movieInfo.model.Movie;
+import jack.movieInfo.model.MovieSummary;
+
+@RestController
+@RequestMapping("movies")
+public class MovieResource {
+
+	@Value("${api.key}")
+	private String apiKey;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping("{movieId}")
+	public Movie getMovie(@PathVariable String movieId) {
+		String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
+		
+		MovieSummary summary = restTemplate.getForObject(url, MovieSummary.class);
+		
+		return new Movie(movieId, summary.getTitle(), summary.getOverview());
+	}
+}
